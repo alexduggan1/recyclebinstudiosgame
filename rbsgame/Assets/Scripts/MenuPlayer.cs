d63f6fe20ef14db0a9abac89c41406e1fb985b13
alexduggan1 <alex.r.duggan@gmail.com>
2024-09-15 17:47:41 -0400

    continued work on menu structure, added a blank inputactions asset to use later

---
+++ rbsgame/Assets/Scripts/MenuPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPlayer : MonoBehaviour
+{
+    // have a thing for the controls here
+
+    public int ID;
+
+    public Character chosenChar;
+
+
+    public UIElm currentUIElm;
+
+    public UIElm.PanelType currentPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ rbsgame/Assets/Scripts/UIElm.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIElm : MonoBehaviour
+{
+    public UIElm elmAbove = null;
+    public UIElm elmBelow = null;
+    public UIElm elmLeft = null;
+    public UIElm elmRight = null;
+
+    public enum PanelType
+    {
+        Character, Stage, ItemWeight, Confirm
+    }
+
+    public PanelType panelType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
